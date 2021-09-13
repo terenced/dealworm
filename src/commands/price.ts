93064e920d0023471fbff92c290210317b5fa1f5
terenced <terry.dellino@gmail.com>
2021-09-13 15:49:18 -0400

    Add fetchOnly option for price command

--- src/commands/price.ts
+++ src/commands/price.ts
@@ -1,3 +1,5 @@
+import isbn3 from "https://cdn.skypack.dev/isbn3";
+
 import { Command } from "../deps/cliffy.ts";
 import * as Fae from "../deps/fae.ts";
 import { wait } from "../deps/wait.ts";
@@ -13,39 +15,66 @@ import { getLogger } from "../utils/logger.ts";
 export const priceCommand = new Command()
   .option("-l, --limit [type:number]", "Limit to process", { default: 3 })
   .option("-a, --all [type:boolean]", "All", { default: false })
-  .option("-i, --isbn [type:string]", "Find a specifc book by ISBN")
-  .action(async ({ limit, verbose, all, isbn }) => {
-    const spinner = wait("Loading data");
-    if (!verbose) spinner.start();
-    const store = getStore();
-    const logger = getLogger(verbose);
-    const items = orderedUnpricedBooks(store);
-    limit = all ? items.length : limit;
-    const books = Fae.take(limit, items);
-
-    let current = 1;
-    for (const book of books) {
-      const bookInfoText = `(${current++}/${limit}) ${book.isbn} ${book.title}`;
-      spinner.text = `Pricing ${bookInfoText}`;
-      logger.info(`${bookInfoText}: Pricing`);
-      try {
-        const am = await findByISBN(book.isbn);
-        const priced = {
-          ...book,
-          ...am,
-          updated: Date.now(),
-        } as StoreRecord;
-        logger.info(`${bookInfoText}: Found ${am.price}`);
-        spinner.text = `Found ${am.price}`;
-        store.updateAndCommit(book.isbn, priced);
-      } catch (error) {
-        logger.error(`${bookInfoText}: ${error}`);
-        store.updateAndCommit(book.isbn, {
-          ...book,
-          updated: Date.now(),
-        });
-      }
+  .option(
+    "-i, --isbn [type:string]",
+    "Find a specifc book by ISBN",
+    (value: string) => isbn3.asIsbn13(value),
+  )
+  .option("--fetchOnly [type:boolean]", "Fetch only and print the info", {
+    default: false,
+  })
+  .action(async ({ limit, verbose, all, isbn, fetchOnly }) => {
+    if (fetchOnly) {
+      await fetchISBNOnly(isbn);
+    } else {
+      await priceBooks(limit, all, isbn, verbose);
     }
-    store.save();
-    spinner.succeed("All done!");
   });
+
+async function priceBooks(
+  limit: number,
+  all: boolean,
+  isbn?: string,
+  verbose?: boolean,
+) {
+  const spinner = verbose ? undefined : wait("Loading data");
+  if (spinner) spinner.start();
+  const store = getStore();
+  const logger = getLogger(verbose);
+  const items = isbn ? [store.get(isbn)] : orderedUnpricedBooks(store);
+  limit = all ? items.length : limit;
+  const books = Fae.take(limit, items);
+
+  let current = 1;
+  for (const book of books) {
+    if (!book) continue;
+    const bookInfoText = `(${current++}/${limit}) ${book.isbn} ${book.title}`;
+    if (spinner) spinner.text = `Pricing ${bookInfoText}`;
+    logger.info(`${bookInfoText}: Pricing`);
+    try {
+      const am = await findByISBN(book.isbn);
+      const priced = {
+        ...book,
+        ...am,
+        updated: Date.now(),
+      } as StoreRecord;
+      logger.info(`${bookInfoText}: Found ${am.price}`);
+      if (spinner) spinner.text = `Found ${am.price}`;
+      store.updateAndCommit(book.isbn, priced);
+    } catch (error) {
+      logger.info(`${bookInfoText}: encountered an error`);
+      logger.error(`${bookInfoText}: ${error}`);
+      store.updateAndCommit(book.isbn, {
+        ...book,
+        updated: Date.now(),
+      });
+    }
+  }
+  store.save();
+  if (spinner) spinner.succeed("All done!");
+}
+
+async function fetchISBNOnly(isbn: string) {
+  const item = await findByISBN(isbn);
+  console.log(item);
+}
