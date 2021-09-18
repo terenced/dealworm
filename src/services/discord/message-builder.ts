913449fd7bcdf5f31ce60afe4842424209dc1b68
terenced <terry.dellino@gmail.com>
2021-09-18 12:20:20 -0400

    Initial port of discord-webhook to deno

---
+++ src/services/discord/message-builder.ts
@@ -0,0 +1,141 @@
+const formatColor = (color: string | number) => {
+  if (typeof color === "string" && color.startsWith("#")) {
+    const rawHex = color.split("#")[1];
+
+    return parseInt(rawHex, 16);
+  } else {
+    return Number(color);
+  }
+};
+
+export type WebhookField = {
+  name: string;
+  value: string;
+  inline?: boolean;
+};
+
+export type WebhookEmbed = {
+  author?: {
+    name?: string;
+    url?: string;
+    icon_url?: string;
+  };
+  title?: string;
+  url?: string;
+  thumbnail?: {
+    url?: string;
+  };
+  image?: {
+    url?: string;
+  };
+  timestamp?: Date;
+  color?: number;
+  description?: string;
+  fields: WebhookField[];
+  footer?: {
+    text: string;
+    icon_url?: string;
+  };
+};
+
+export type WebhookPayload = {
+  content: string;
+  embeds: WebhookEmbed[];
+};
+
+export default class MessageBuilder {
+  private payload: WebhookPayload;
+  constructor() {
+    this.payload = {
+      embeds: [{ fields: [] }],
+      content: "",
+    };
+  }
+
+  getJSON() {
+    return this.payload;
+  }
+
+  setText(text: string) {
+    this.payload.content = text;
+
+    return this;
+  }
+
+  setAuthor(author: string, authorImage?: string, authorUrl?: string) {
+    this.payload.embeds[0].author = {
+      name: author,
+      url: authorUrl ?? "",
+      icon_url: authorImage,
+    };
+
+    return this;
+  }
+
+  setTitle(title: string) {
+    this.payload.embeds[0].title = title;
+
+    return this;
+  }
+
+  setURL(url: string) {
+    this.payload.embeds[0].url = url;
+
+    return this;
+  }
+
+  setThumbnail(thumbnail: string) {
+    this.payload.embeds[0].thumbnail = {};
+    this.payload.embeds[0].thumbnail.url = thumbnail;
+
+    return this;
+  }
+
+  setImage(image: string) {
+    this.payload.embeds[0].image = {};
+    this.payload.embeds[0].image.url = image;
+
+    return this;
+  }
+
+  setTimestamp(date?: Date) {
+    if (date) {
+      this.payload.embeds[0].timestamp = date;
+    } else {
+      this.payload.embeds[0].timestamp = new Date();
+    }
+
+    return this;
+  }
+
+  setColor(color: string | number) {
+    this.payload.embeds[0].color = formatColor(color);
+
+    return this;
+  }
+
+  setDescription(description: string) {
+    this.payload.embeds[0].description = description;
+
+    return this;
+  }
+
+  addField(fieldName: string, fieldValue: string, inline = false) {
+    this.payload.embeds[0].fields.push({
+      name: fieldName,
+      value: fieldValue,
+      inline: inline,
+    });
+
+    return this;
+  }
+
+  setFooter(footer: string, footerImage = "") {
+    this.payload.embeds[0].footer = {
+      icon_url: footerImage,
+      text: footer,
+    };
+
+    return this;
+  }
+}
---
+++ src/services/discord/webhook.ts
@@ -0,0 +1,81 @@
+import MessageBuilder, { WebhookPayload } from "./message-builder.ts";
+type WebhookOptions = {
+  url: string;
+  throwErrors?: boolean;
+  retryOnLimit?: boolean;
+};
+
+export default class Webhook {
+  private url: string;
+  private throwErrors?: boolean;
+  private retryOnLimit?: boolean;
+  private payload: any = {};
+
+  constructor(options: string | WebhookOptions) {
+    this.payload = {};
+
+    if (typeof options == "string") {
+      this.url = options;
+      this.throwErrors = true;
+      this.retryOnLimit = true;
+    } else {
+      this.url = options.url;
+      this.throwErrors = options.throwErrors == undefined
+        ? true
+        : options.throwErrors;
+      this.retryOnLimit = options.retryOnLimit == undefined
+        ? true
+        : options.retryOnLimit;
+    }
+  }
+
+  setUsername(username: string) {
+    this.payload.username = username;
+
+    return this;
+  }
+
+  setAvatar(avatarURL: string) {
+    this.payload.avatar_url = avatarURL;
+
+    return this;
+  }
+
+  private async post(payload: WebhookPayload) {
+    return await fetch(this.url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    });
+  }
+
+  async send(content: MessageBuilder | string) {
+    let payload: WebhookPayload;
+
+    if (typeof content === "string") {
+      payload = new MessageBuilder().setText(content).getJSON();
+    } else {
+      payload = content.getJSON();
+    }
+
+    try {
+      const res = await this.post(payload);
+
+      if (res.status === 429 && this.retryOnLimit) {
+        const body = await res.json();
+        const waitUntil = body["retry_after"];
+
+        setTimeout(() => this.post(payload), waitUntil);
+      } else if (res.status != 204) {
+        throw new Error(
+          `Error sending webhook: ${res.status} status code. Response: ${await res
+            .text()}`,
+        );
+      }
+    } catch (err) {
+      if (this.throwErrors) throw new Error(err.message);
+    }
+  }
+}
